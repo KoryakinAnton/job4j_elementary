6a0973f097139abce08914bf3990b7f15aa913a5
Anton <anton.koryakin@mail.ru>
2021-02-01 22:42:15 +0300

    4.3.6. Math.abs [#460073]

---
+++ src/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2) {
+        int x = Math.abs(x2 - x1);
+        int y = Math.abs(y2 - y1);
+        if (x == y) {
+            return x;
+        } else {
+            return 0;
+        }
+    }
+}
